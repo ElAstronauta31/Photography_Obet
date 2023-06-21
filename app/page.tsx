536f33ad7d0130b82d4fce9854f6b1478b25359e
ElAstronauta31 <72705551+ElAstronauta31@users.noreply.github.com>
2023-06-20 20:50:40 -0400

    added styles for wedding images using grids. Need to clean up a bit

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from '../styles/page.module.css'
 import Nav from '../comps/nav'
 import Landing from '../comps/landing'
+import Weddings from '../comps/weddings'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Nav />
-      <Landing />
+      {/* <Nav />
+      <Landing /> */}
+      <Weddings />
     </main>
   )
 }
--- comps/weddings.js
+++ comps/weddings.js
@@ -1,9 +1,42 @@
-
+import Image from 'next/image'
+import styles from '../styles/weddings.module.css'
 
 export default function Weddings() {
     return(
-        <div>
-            
+        <div className={styles.grid}>
+            <div className={styles.div}>
+                    <Image src='/images/weddings/galuh-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+            </div>
+            <div className={styles.div}>
+                    <Image src='/images/weddings/joeyy-wedding2.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+            </div>
+            <div className={styles.horizontalDiv}>
+                <div className={styles.horizontal}>
+                <Image src='/images/weddings/samantha-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+            </div>
+            <div className={styles.divLeft}>
+                <div className={styles.div2}>
+                    <Image src='/images/weddings/thomas-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+                <div className={styles.div}>
+                    <Image src='/images/weddings/joeyy-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+                <div className={styles.div}>
+                    <Image src='/images/weddings/luwadlin-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+            </div>
+            <div className={styles.divRight}>
+                <div className={styles.div}>
+                    <Image src='/images/weddings/paniz-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+                <div className={styles.div3}>
+                    <Image src='/images/weddings/monia-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+                <div className={styles.div}>
+                    <Image src='/images/weddings/elisa-wedding.jpg' fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className={styles.img}/>
+                </div>
+            </div>
         </div>
     )
 }
